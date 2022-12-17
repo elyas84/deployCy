abca9ad2ce8a3cae2e44c0af6c0a543c3f9973cd
elyas <elyas.arkin@hotmail.se>
2022-12-17 20:42:51 +0100

    updated

--- server/server.js
+++ server/server.js
@@ -2,28 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// e.g data
 
-const data = [
-  {
-    name: "Elyas",
-    age: 19,
-    gender: "male",
-  },
-  {
-    name: "Jesur",
-    age: 11,
-    gender: "male",
-  },
-  {
-    name: "Öykur",
-    age: 6,
-    gender: "male",
-  },
-];
-
-app.get("/api/hello", (req, res) => {
-  res.json(data);
+app.get("/", (req, res) => {
+  res.json("Hello TEST");
 });
 
 app.listen(PORT, () => {
